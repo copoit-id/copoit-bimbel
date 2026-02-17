074e144a8508e8b1738b053dd6baefa64a46e852
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2026-02-17 14:03:58 +0700

    fix: update preview katex in pembahsan user

--- resources/views/user/layout/user.blade.php
+++ resources/views/user/layout/user.blade.php
@@ -7,6 +7,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $clientBranding['name'] }}</title>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
+    <script>
+        window.MathJax = {
+            skipStartupTypeset: true,
+            tex2jax: {
+                inlineMath: [['$', '$'], ['\\(', '\\)']],
+                displayMath: [['$$', '$$'], ['\\[', '\\]']],
+                processEscapes: true
+            },
+            messageStyle: 'none',
+            showMathMenu: false,
+            'HTML-CSS': {
+                availableFonts: ['TeX']
+            }
+        };
+    </script>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/MathJax.js?config=TeX-AMS_HTML"></script>
     @vite('resources/css/app.css')
     @include('components.branding-styles')
     @include('components.favicon-link')
@@ -25,6 +41,21 @@
     {{-- jquery --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    <script>
+        window.renderMathJax = function () {
+            if (window.MathJax) {
+                if (window.MathJax.Hub) {
+                    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+                } else if (window.MathJax.typesetPromise) {
+                    MathJax.typesetPromise();
+                }
+            }
+        };
+
+        document.addEventListener('DOMContentLoaded', function () {
+            window.renderMathJax();
+        });
+    </script>
 
     @vite('resources/js/app.js')
     @yield('scripts')
